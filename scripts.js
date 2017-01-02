e4a2d3b2e79b2241f00da2ee6f4a381cbffc1436
adamShaleen <adamshaleen@gmail.com>
2017-01-01 20:55:13 -0600

    sorting JSON object into 2 seperate arrays based on online status.

--- scripts.js
+++ scripts.js
@@ -3,20 +3,28 @@ $(document).ready(function() {
     // workaround to twitch TV's new key protocol from FCC
     var api = "api.js";
 
+    var online = [];
+    var offline = [];
+
     $.getJSON(api, function(data) {
 
-        console.log(data);
+        // loop through entire API object and sort into corresponding array
+        for (var i = 0; i < data.length; i++) {
+            if (!data[i].stream) {
+                offline.push(data[i]);
+            } else {
+                online.push(data[i]);
+            }
+        }
 
-        $("#accountLogo").attr("src", data[0].stream.logo);
-        $("#accountName").html(data[0].stream.display_name);
-        $("#accountStatus").html(data[0].stream.status);
-        $("#accountLink").attr("href", data[0].stream.url);
+        console.log(online, offline);
+
+        // $("#accountLogo").attr("src", data[0].stream.logo);
+        // $("#accountName").html(data[0].stream.display_name);
+        // $("#accountStatus").html(data[0].stream.status);
+        // $("#accountLink").attr("href", data[0].stream.url);
 
     });
-
-
-
-
 
 
 
