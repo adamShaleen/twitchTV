812f01be7f3ada11f3eb9d40e5286ee2d5a92c10
adamShaleen <adamshaleen@gmail.com>
2017-01-03 18:36:42 -0600

    used .each loop for online and offline users and added respective HTML elements and attributes.

--- scripts.js
+++ scripts.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
     // workaround to twitch TV's new key protocol from FCC
     var api = "api.js";
 
-    var online = [];
-    var offline = [];
-
     $.getJSON(api, function(data) {
 
-        // loop through entire API object and sort into corresponding array
+        // containers for 2 different channel types
+        var online = [];
+        var offline = [];
+
+        // loop through entire API object and sort into corresponding channel type array
         for (var i = 0; i < data.length; i++) {
             if (!data[i].stream) {
                 offline.push(data[i]);
@@ -17,13 +18,28 @@ $(document).ready(function() {
             }
         }
 
-        console.log(online, offline);
+        // repeat element with each of the 4 data pieces of JSON object data for each object in  online array
+        $.each(online, function() {
+            $("<img></img>").appendTo("#online").attr({src: this.stream.logo, class: 'accountLogo'});
+            $("<p>" + this.stream.display_name + "</p>").appendTo("#online").attr({class: 'accountName'});
+            $("<p>" + this.stream.status + "</p>").appendTo("#online").attr({class: 'accountStatus'});
+            $("<a>LINK</a>").appendTo("#online").attr({target: "_blank",href: this.stream.url, class: 'accountLink'});
+            $("<br>").appendTo("#online");
+            $("<hr>").appendTo("#online");
+        });
 
-        // $("#accountLogo").attr("src", data[0].stream.logo);
-        // $("#accountName").html(data[0].stream.display_name);
-        // $("#accountStatus").html(data[0].stream.status);
-        // $("#accountLink").attr("href", data[0].stream.url);
+        // repeat element with each of the 2 data pieces of JSON object data for each object in  offline array
+        $.each(offline, function() {
+            if (this.display_name) {
+                $("<p>" + this.display_name + "</p>").appendTo("#offline").attr({class: 'accountName'});
+                $("<a>LINK</a>").appendTo("#offline").attr({target: "_blank",href: this._links.channel, class: 'accountLink'});
+                $("<br>").appendTo("#offline");
+                $("<hr>").appendTo("#offline");
+            } else {
+                return;
+            }
 
+        });
     });
 
 
